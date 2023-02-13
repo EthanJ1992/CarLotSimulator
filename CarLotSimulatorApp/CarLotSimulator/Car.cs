d4a015c12703c7ec670bdf6d03fff921214faacc
Ethan Jacobs <jacobethan1992@gmail.com>
2023-02-13 16:59:41 -0500

    CarLotExercise

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    internal class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public void MakeEngineNoise(string engineNoise)
+        {
+            Console.WriteLine($"my car sounds like {engineNoise}" );
+        }
+        public void MakeHonkNoise(string honkNoise)
+        {
+            Console.WriteLine($"my car horn sounds like {honkNoise}");
+        }
+    }
+}
+
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -18,7 +18,50 @@ namespace CarLotSimulator
             //Set the properties for each of the cars
             //Call each of the methods for each car
 
+            Car myCar = new Car();
+            Car ourCar= new Car();
+            Car yourCar = new Car();
+    
+            myCar.EngineNoise = "vroom";
+            myCar.HonkNoise = "beep beep";
+            myCar.IsDriveable = true;
+            myCar.Year = 2022;
+            myCar.Make = "Honda";
+            myCar.Model = "Accord";
+            myCar.MakeEngineNoise(myCar.EngineNoise);
+            myCar.MakeHonkNoise(myCar.HonkNoise);
+            
+            ourCar.EngineNoise = "vroom";
+            ourCar.HonkNoise = "beep beep";
+            ourCar.IsDriveable = true;
+            ourCar.Year = 2022;
+            ourCar.Make = "Honda";
+            ourCar.Model = "Accord";
+            ourCar.MakeEngineNoise(ourCar.EngineNoise);
+            ourCar.MakeHonkNoise(ourCar.HonkNoise);
+
+            yourCar.EngineNoise = "vroom";
+            yourCar.HonkNoise = "beep beep";
+            yourCar.IsDriveable = true;
+            yourCar.Year = 2022;
+            yourCar.Make = "Honda";
+            yourCar.Model = "Accord";
+            yourCar.MakeEngineNoise(yourCar.EngineNoise);
+            yourCar.MakeHonkNoise(yourCar.HonkNoise);
+
+
+
+
+
+
+
+
+
+
+            our.Make = "";
+
             //*************BONUS*************//
+
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
 
